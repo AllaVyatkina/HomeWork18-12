8d7bbf4f37a047a68779fe1cabf40853d6bcaaaf
Alla <bakrlona@mail.ru>
2022-12-20 23:01:49 +0500

    Ex1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿  Console.WriteLine ("Введите трехзначное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if ((number > 100) && (number < 1000))
+    {
+     number = number % 100 / 10;
+
+    Console.WriteLine(number);
+    }
+    else 
+    {
+      Console.WriteLine("Введено не трехзначное число");
+    }
