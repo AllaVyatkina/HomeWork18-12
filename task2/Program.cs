8aeac624b3beb899e8b3d076d2b2dfe7f77e55c0
Alla <bakrlona@mail.ru>
2022-12-20 23:05:41 +0500

    Ex2

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿   Console.WriteLine ("Введите число");
+
+        string number = Console.ReadLine();
+        if(number.Length < 3) 
+        {
+            Console.WriteLine("Третьей цифры нет");
+          return;
+        }
+
+        Console.WriteLine(number[2]);
