cb9f0d3a0a9a074001a4548d58e52083c3e4acfd
Alla <bakrlona@mail.ru>
2022-12-20 23:12:49 +0500

    Ex3

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите день недели от 1 до 7: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+    if(number >8 || number <1)
+    {
+    Console.WriteLine("Введено некоректное значение");
+    return;
+
+    }
+    if(number >5)
+    {
+    Console.WriteLine("Выходной"); 
+    }
+    else
+    {
+    Console.WriteLine("Будни");
+    }
